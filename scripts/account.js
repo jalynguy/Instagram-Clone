e887078d2000e4fdf7cfe2c77833198ab6354fd0
Andrea M <andrea.ramirez@andrea.ramirez-MacBook-Pro>
2024-10-24 11:41:44 -0700

    cleaner code, added profile pic

--- scripts/account.js
+++ scripts/account.js
@@ -1,15 +1,24 @@
 
-// create an array of 24 images
 const postsContainer = document.getElementById("posts");
+const profileHeader = document.querySelector(".profile-img");
+const followers = document.querySelector(".number");
+let numberOfPosts = Math.floor(Math.random() * 48);
 
-function getImage() {
+followers.innerHTML = numberOfPosts;
+
+//     profileHeader.insertBefore(profilePic, profileHeader.querySelector('.profile-info'));
+
+function getImg(classToAdd, container) {
     let img = new Image();
     const random = Math.floor(Math.random() * 10000);
     img.src = `https://loremflickr.com/200/200?random=${random}`;
-    img.classList.add("post");
 
-    postsContainer.appendChild(img);
+    img.classList.add(classToAdd);
+    container.appendChild(img);
 }
-getImage();
-getImage();
-getImage();
+
+getImg("profile-pic", profileHeader);
+
+for (let i = 0; i < numberOfPosts; i++) {
+    getImg("post", postsContainer);
+}
